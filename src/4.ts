96e4af1436c3a37334eadfe9d7b8334864111398
Leskovets Maksym <leskovecmaksim@gmail.com>
2024-02-17 01:04:42 +0100

    Finished hw7

--- src/4.ts
+++ src/4.ts
@@ -1,3 +1,40 @@
+class Key {
+    private signature: number;
+    constructor() {
+        this.signature = Math.floor(Math.random() * 101);
+    }
+    
+    public getSignature(): number {
+        return this.signature;
+    }
+}
+
+class Person {
+    constructor(private key = new Key) { }
+
+    getKey() {
+        return this.key;
+    }
+}
+
+abstract class House {
+    private tenants: Person[] = [];
+    protected door = true;
+
+    constructor(private key: Key) { }
+
+    comeIn(tenant: Person): void {
+        this.door && this.tenants.push(tenant);
+    }
+}
+
+class MyHouse extends House {
+
+    openDoor(randomKey: Key): void {
+        this.door = randomKey.getSignature() === key.getSignature();
+    } 
+}
+
 const key = new Key();
 
 const house = new MyHouse(key);
